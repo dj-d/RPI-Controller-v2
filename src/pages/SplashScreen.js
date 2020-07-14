a1b1977734bad7ed581a91807c14b349c2e6b23a
Daniele Albanese <daniele.albanese98@gmail.com>
2020-07-15 01:20:09 +0200

    Added page SplashScreen.js

---
+++ src/pages/SplashScreen.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, ActivityIndicator} from 'react-native';
+
+import Logo from '../components/Logo'
+
+function SplashScreen() {
+	return (
+		<View style={styles.container}>
+			<Logo/>
+			<ActivityIndicator size="large" color="rgba(106, 191, 75, 0.9)"/>
+		</View>
+	);
+}
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1/3
+	}
+})
