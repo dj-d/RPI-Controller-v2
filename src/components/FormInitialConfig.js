07bb6f154753c75da22516a2073824d0bef227a6
Daniele Albanese <daniele.albanese98@gmail.com>
2020-07-17 18:53:55 +0200

    Added formStyles.js for styles of forms components

--- src/components/FormInitialConfig.js
+++ src/components/FormInitialConfig.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 
+import formStyles from '../styles/formStyles'
+
 export default class FormSignUp extends Component {
 	render() {
 		return (
@@ -27,38 +29,5 @@ export default class FormSignUp extends Component {
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flexGrow: 1,
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-
-	textInput: {
-		width: 300,
-		height: 40,
-		backgroundColor: 'rgba(255, 255, 255, 0.3)',
-		borderRadius: 25,
-		marginVertical: 5,
-		paddingHorizontal: 15,
-		// underlineColorAndroid: 'rgba(0, 0, 0, 0)',
-		fontSize: 16,
-		color: 'rgba(255, 255, 255, 0.7)',
-		// placeholderTextColor: 'rgba(255, 255, 255, 0.7)'
-	},
-
-	button: {
-		width: 300,
-		height: 40,
-		backgroundColor: 'rgba(106, 191, 75, 0.9)',
-		borderRadius: 25,
-		marginVertical: 5
-	},
-
-	buttonText: {
-		fontSize: 16,
-		fontWeight:'500',
-		color: 'rgba(255, 255, 255, 0.7)',
-		textAlign: 'center',
-		paddingVertical: 12
-	}
+	...formStyles
 })
---
+++ src/styles/formStyles.js
@@ -0,0 +1,36 @@
+export default {
+	container: {
+		flexGrow: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+
+	textInput: {
+		width: 300,
+		height: 40,
+		backgroundColor: 'rgba(255, 255, 255, 0.3)',
+		borderRadius: 25,
+		marginVertical: 5,
+		paddingHorizontal: 15,
+		// underlineColorAndroid: 'rgba(0, 0, 0, 0)',
+		fontSize: 16,
+		color: 'rgba(255, 255, 255, 0.7)',
+		// placeholderTextColor: 'rgba(255, 255, 255, 0.7)'
+	},
+
+	button: {
+		width: 300,
+		height: 40,
+		backgroundColor: 'rgba(106, 191, 75, 0.9)',
+		borderRadius: 25,
+		marginVertical: 5
+	},
+
+	buttonText: {
+		fontSize: 16,
+		fontWeight:'500',
+		color: 'rgba(255, 255, 255, 0.7)',
+		textAlign: 'center',
+		paddingVertical: 12
+	}
+}
