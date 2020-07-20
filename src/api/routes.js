c2fe84d8a7320c9a7fb5ee68aa2a6ef275d40ef3
Daniele Albanese <daniele.albanese98@gmail.com>
2020-07-20 21:04:26 +0200

    routes.js -> Added functions, added TODO and added partial DOC

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,5 +1,10 @@
 import { AsyncStorage } from 'react-native'
 
+/**
+ *
+ *
+ * @returns {Promise<*>}
+ */
 async function getHost() {
 	return await AsyncStorage.getItem("conSettings")
 		.then((data) => {
@@ -9,6 +14,11 @@ async function getHost() {
 		});
 }
 
+/**
+ *
+ *
+ * @returns {Promise<boolean>}
+ */
 async function connectionTest() {
 	return await fetch(await getHost() + '/', {method: 'GET'})
 		.then((response) => response.json())
@@ -22,18 +32,88 @@ async function connectionTest() {
 		})
 }
 
-async function signUp() {
-
+/**
+ *
+ *
+ * @param data
+ * @returns {Promise<any>}
+ */
+async function signUp(data) {
+	return (await fetch(await getHost() + '/signup', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	})).json()
 }
 
-async function otpRequest() {
-
+/**
+ *
+ *
+ * @param data
+ * @returns {Promise<any>}
+ */
+async function otpRequest(data) {
+	return (await fetch(await getHost() + '/otp_request', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	})).json()
 }
 
-async function login() {
-
+/**
+ *
+ *
+ * @param data
+ * @returns {Promise<any>}
+ */
+async function login(data) {
+	return await fetch(await getHost() + '/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	}).json()
 }
 
+/**
+ *
+ *
+ * @param data
+ * @returns {Promise<void>}
+ */
+async function changePassword(data) {
+	// TODO
+}
+
+/**
+ *
+ *
+ * @param data
+ * @returns {Promise<void>}
+ */
+async function resetPassword(data) {
+	// TODO
+}
+
+/**
+ *
+ *
+ * @param data
+ * @returns {Promise<void>}
+ */
+async function device(data) {
+	// TODO
+}
+
+
+/**
+ * Export functions
+ */
 export default {
 	connectionTest
 }
