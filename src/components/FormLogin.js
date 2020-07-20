3861957ad3910a93940027e07ea7cd3736a39615
Daniele Albanese <daniele.albanese98@gmail.com>
2020-07-20 21:03:51 +0200

    FormLogin.js + FormSignUp.js -> get value of textInput

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -4,17 +4,24 @@ import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native'
 import formStyles from "../styles/formStyles";
 
 export default class FormLogin extends Component {
+	data = {
+		email: '',
+		password: ''
+	}
+
 	render() {
 		return (
 			<View style={styles.container}>
 				<TextInput
 					style={styles.textInput}
 					placeholder="Email"
+					onChangeText={(email) => this.setState({email: email})}
 				/>
 				<TextInput
 					style={styles.textInput}
 					placeholder="Password"
 					secureTextEntry={true}
+					onChangeText={(password) => this.setState({password: password})}
 				/>
 
 				<TouchableOpacity style={styles.button}>
--- src/components/FormSignUp.js
+++ src/components/FormSignUp.js
@@ -4,33 +4,51 @@ import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native'
 import formStyles from "../styles/formStyles";
 
 export default class FormSignUp extends Component {
+	data = {
+		name: '',
+		surname: '',
+		username: '',
+		email: '',
+		password: ''
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		// TODO: Add check of password
+	}
+
 	render() {
 		return (
 			<View style={styles.container}>
 				<TextInput
 					style={styles.textInput}
 					placeholder="Name"
+					onChangeText={(name) => this.setState({name: name})}
 				/>
 				<TextInput
 					style={styles.textInput}
 					placeholder="Surname"
+					onChangeText={(surname) => this.setState({surname: surname})}
 				/>
 				<TextInput
 					style={styles.textInput}
 					placeholder="Username"
+					onChangeText={(username) => this.setState({username: username})}
 				/>
 				<TextInput
 					style={styles.textInput}
 					placeholder="Email"
+					onChangeText={(email) => this.setState({email: email})}
 				/>
 				<TextInput
 					style={styles.textInput}
 					placeholder="Password"
 					secureTextEntry={true}
+					onChangeText={(password) => this.setState({password: password})}
 				/>
 				<TextInput
 					style={styles.textInput}
 					placeholder="Confirm password"
+					secureTextEntry={true}
 				/>
 
 				<TouchableOpacity style={styles.button}>
